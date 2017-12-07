9b53b8f198f38c92ed85b97f4f860e4f931b4655
Binbin Zhang <811364747@qq.com>
2017-12-07 21:07:20 +0800

    add matrix and vector

--- tensor.cc
+++ tensor.cc
@@ -1,102 +1,81 @@
 /* Created on 2017-12-05
  * Author: Binbin Zhang
  */
-
-
-
 #include "tensor.h"
 
-
-template <class DType>
-void Tensor<DType>::Resize(std::vector<int> shape) {
-    if (data_ != NULL) delete [] data_;
+template <class DType, int32_t Dim>
+void Tensor<DType, Dim>::Resize(const std::vector<int32_t> &shape) {
+    CHECK(shape_.size() == Dim);
+    CHECK(shape.size() == Dim);
+    int32_t size = GetShapeSize(shape);
+    if (size == 0 || size == this->Size()) return;
+    if (holder_ && data_ != nullptr) delete [] data_;
     shape_ = shape;
-    int size = this->Size();
-    if (size == 0) return;
     data_ = new DType[size]();
+    holder_ = true;
 }
 
-template <class DType>
-void Tensor<DType>::Resize(int dim) {
-    std::vector<int> shape = {dim};
-    Resize(shape);
+template <class DType, int32_t Dim>
+int32_t Tensor<DType, Dim>::GetShapeSize(const std::vector<int32_t> &shape) const {
+    if (shape.size() == 0) return 0;
+    int32_t size = 1;
+    for (int32_t i = 0; i < shape.size(); i++) size *= shape[i];
+    return size;
 }
 
-template <class DType>
-void Tensor<DType>::Resize(int rows, int cols) {
-    std::vector<int> shape = {rows, cols};
-    Resize(shape);
-}
-
-template <class DType>
-void Tensor<DType>::FromProto(const TensorProto &proto) {
-    shape_.resize(proto.shape_size());
+template <class DType, int32_t Dim>
+void Tensor<DType, Dim>::FromProto(const TensorProto &proto) {
+    CHECK(shape_.size() == Dim);
+    CHECK(proto.shape_size() == Dim);
+    std::vector<int32_t> shape(Dim, 0);
     for (int i = 0; i < proto.shape_size(); i++) {
-        shape_[i] = proto.shape(i);
+        shape[i] = proto.shape(i);
     }
-    Resize(shape_);
+    Resize(shape);
+    // Check type
+    CHECK(ParseType<DType>::Type() == proto.data_type());
     if (proto.data_type() == TensorProto::FLOAT) {
         for (int i = 0; i < proto.float_data_size(); i++) {
             data_[i] = proto.float_data(i);
         }
     }
-    else if (proto.data_type() == TensorProto::DOUBLE) {
-        for (int i = 0; i < proto.double_data_size(); i++) {
-            data_[i] = proto.double_data(i);
-        }
-    }
     else if (proto.data_type() == TensorProto::INT32 || 
              proto.data_type() == TensorProto::INT16 ||
              proto.data_type() == TensorProto::INT8) {
-         for (int i = 0; i < proto.int32_data_size(); i++) {
+        for (int i = 0; i < proto.int32_data_size(); i++) {
             data_[i] = static_cast<DType>(proto.int32_data(i));
-         }
+        }
     }
     else {
         ERROR("Not implement");
     }
 }
 
-template <>
-void Tensor<float>::ToProto(TensorProto *proto) const {
+template <class DType, int32_t Dim>
+void Tensor<DType, Dim>::ToProto(TensorProto *proto) const {
     proto->clear_shape();
     for (int i = 0; i < shape_.size(); i++) {
         proto->add_shape(shape_[i]);
     }
-    proto->set_data_type(TensorProto::FLOAT);
-    proto->clear_float_data();
-    for (int i = 0; i < Size(); i++) {
-        proto->add_float_data(data_[i]);   
+    TensorProto_DataType data_type = ParseType<DType>::Type();
+    proto->set_data_type(data_type);
+    if (data_type == TensorProto::FLOAT) {
+        proto->clear_float_data();
+        for (int i = 0; i < Size(); i++) {
+            proto->add_float_data(data_[i]);
+        }
+    }
+    else if (data_type == TensorProto::INT32 || 
+             data_type == TensorProto::INT16 ||
+             data_type == TensorProto::INT8) {
+        proto->clear_int32_data();
+        for (int i = 0; i < Size(); i++) {
+            proto->add_int32_data(static_cast<int32_t>(data_[i]));
+        }
+    }
+    else {
+        ERROR("Not implement");
     }
 }
 
-template <>
-void Tensor<int>::ToProto(TensorProto *proto) const {
-    proto->clear_shape();
-    for (int i = 0; i < shape_.size(); i++) {
-        proto->add_shape(shape_[i]);
-    }
-    proto->set_data_type(TensorProto::INT32);
-    proto->clear_int32_data();
-    for (int i = 0; i < Size(); i++) {
-        proto->add_int32_data(data_[i]);   
-    }
-}
-
-template <>
-void Tensor<uint8_t>::ToProto(TensorProto *proto) const {
-    proto->clear_shape();
-    for (int i = 0; i < shape_.size(); i++) {
-        proto->add_shape(shape_[i]);
-    }
-    proto->set_data_type(TensorProto::INT8);
-    proto->clear_int32_data();
-    for (int i = 0; i < Size(); i++) {
-        proto->add_int32_data(data_[i]);   
-    }
-}
-
-template class Tensor<float>;
-template class Tensor<int>;
-template class Tensor<uint8_t>;
 
--- tensor.h
+++ tensor.h
@@ -12,27 +12,82 @@
 #include "net.pb.h"
 
 template <class DType>
+class ParseType {
+    static TensorProto_DataType Type() { return TensorProto::UNDEFINED; }
+};
+
+#define PARSE_TYPE(type, data_type) \
+template <> \
+TensorProto_DataType ParseType<type>::Type() { return TensorProto::data_type; }
+
+PARSE_TYPE(float, FLOAT)
+PARSE_TYPE(int32_t, INT32)
+PARSE_TYPE(uint8_t, INT8)
+
+template <class DType, int32_t Dim>
 class Tensor {
 public:
-    Tensor(): data_(NULL) {}
+    Tensor(): data_(nullptr), shape_(Dim, 0), holder_(true) {}
     ~Tensor() {
-        if (data_ != NULL) delete [] data_;
+        if (data_ != nullptr) delete [] data_;
     }
     virtual void FromProto(const TensorProto &proto); 
     virtual void ToProto(TensorProto *proto) const;
-    void Resize(std::vector<int> shape);
-    void Resize(int dim);
-    void Resize(int rows, int cols);
-    int Size() const {
-        if (shape_.size() == 0) return 0;
-        int size = 1;
-        for (int i = 0; i < shape_.size(); i++) size *= shape_[i];
-        return size;
+    void Resize(const std::vector<int32_t> &shape);
+    int32_t Size() const {
+        GetShapeSize(shape_);
     }
-private:
-private:
+    DType *Data() const { return data_; } 
+protected:
+    int32_t GetShapeSize(const std::vector<int32_t> &shape) const;
+protected:
     DType *data_;
-    std::vector<int> shape_;
+    std::vector<int32_t> shape_;
+    bool holder_;
+};
+
+template <class DType>
+class Matrix : public Tensor<DType, 2> {
+public:
+    Matrix(int32_t row = 0, int32_t col = 0) {
+        Resize(row, col);
+    }
+    void Resize(int32_t row,int32_t col) {
+        std::vector<int32_t> shape = { row, col };
+        Resize(shape);
+    }
+    int32_t NumRows() const { return this->shape_[0]; }
+    int32_t NumCols() const { return this->shape_[1]; }
+    const DType operator () (int r, int c) const {
+        CHECK(r < NumRows());
+        CHECK(c < NumCols());
+        return *(this->data_ + r * NumCols() + c);
+    }
+    DType& operator () (int r, int c) {
+        CHECK(r < NumRows());
+        CHECK(c < NumCols());
+        return *(this->data_ + r * NumCols() + c);
+    }
+};
+
+template <class DType>
+class Vector: public Tensor<DType, 1> {
+public:
+    Vector(int32_t dim = 0) {
+        Resize(dim);
+    }
+    void Resize(int32_t dim) {
+        std::vector<int32_t> shape = { dim };
+        Resize(shape);
+    }
+    const DType operator () (int n) const {
+        CHECK(n < this->Size());
+        return *(this->data_ + n);
+    }
+    DType& operator () (int n) {
+        CHECK(n < this->Size());
+        return *(this->data_ + n);
+    }
 };
 
 
